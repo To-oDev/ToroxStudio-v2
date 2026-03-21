c74b87f1033e138173b2cf0a57e1cc3bbe779386
to-o <antoniosanchez453@gmail.com>
2026-03-21 15:36:11 -0600

    Tarjetas de integrantes interactivas

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ import './cursor.js';
 import './navigation.js';
 import './reveal.js';
 import { initMobileMenu } from './mobile-menu.js';
+import { initTeamModal } from './team-modal.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     initMobileMenu();
+    initTeamModal();
 });
 
